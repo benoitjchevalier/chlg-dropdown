3e53fadc2000785b5934048bea32902abc23b0e7
Benoit Chevalier <benoi.j.chevalier@gmail.com>
2018-04-14 16:46:40 -0700

    first working version, items, multi mode

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+'use strict';
+const path = require('path');
+const gulp = require('gulp');
+const pkg = require('./package.json');
+const $ = require('gulp-load-plugins')();
+const gulpSequence = require('gulp-sequence');
+const importOnce = require('node-sass-import-once');
+const stylemod = require('gulp-style-modules');
+const gulpif = require('gulp-if');
+const combiner = require('stream-combiner2');
+const argv = require('yargs').argv;
+
+const sassOptions = {
+  importer: importOnce,
+  importOnce: {
+    index: true,
+    bower: true
+  }
+};
+
+gulp.task('clean', function() {
+  return gulp.src(['.tmp', 'css'], {
+    read: false
+  }).pipe($.clean());
+});
+
+function handleError(err){
+  console.log(err.toString());
+  this.emit('end');
+}
+
+function buildCSS(){
+  return combiner.obj([
+    $.sass(sassOptions),
+    $.autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+      flexbox: false
+    }),
+    gulpif(!argv.debug, $.cssmin())
+  ]).on('error', handleError);
+}
+
+gulp.task('sass', function() {
+  return gulp.src(['./sass/*.scss'])
+    .pipe(buildCSS())
+    .pipe(stylemod({
+      moduleId: function(file) {
+        return path.basename(file.path, path.extname(file.path)) + '-styles';
+      }
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['sass/*.scss'], ['sass']);
+});
